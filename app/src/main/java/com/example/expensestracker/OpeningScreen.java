254ee3ed65ec502bfa02347b05b9465411be2d57
MUHAMMAD ZUHAIRY BIN RAZALY <zuhairy812@gmail.com>
2025-01-11 22:43:28 +0800

    Done Tambah Opening Screen

---
+++ app/src/main/java/com/example/expensestracker/OpeningScreen.java
@@ -0,0 +1,33 @@
+package com.example.expensestracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class OpeningScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_opening_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Delay for 3 seconds before launching MainActivity
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(OpeningScreen.this, MainActivity.class);
+            startActivity(intent);
+            finish(); // Close the splash activity
+        }, 3000); // 3000 milliseconds = 3 seconds
+    }
+}
